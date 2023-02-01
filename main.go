9d62be9b0f54d4685c5d053a46bd383ed56df5aa
herrluk <herrluk@example.com>
2023-02-01 21:32:49 +0800

    新增关于界面

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import "fmt"
 import "net/http"
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintln(w, "<h1>Hello, 这里是 goblog </h1>")
-	if err != nil {
-		return
+
+	if r.URL.Path == "/" {
+		fmt.Fprintln(w, "<h1>Hello, 这里是 goblog </h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprintln(w, "此博客是用以记录编程笔记，如您有反馈或建议，"+
+			"请联系<a href=\\\"mailto:herrluk@example.com\\\">herrluk@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
 	}
+
 }
 
 func main() {
