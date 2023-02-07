51a2e56cb7dd8a68c28f500f8c945d8c29aabb44
herrluk <herrluk@example.com>
2023-02-07 15:55:37 +0800

    新建 article 模型

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"goBlogByMyself/pkg/route"
 	"goBlogByMyself/routes"
 
 	"github.com/gorilla/mux"
@@ -10,5 +11,8 @@ import (
 func SetupRoute() *mux.Router {
 	router := mux.NewRouter()
 	routes.RegisterWebRoutes(router)
+
+	route.SetRoute(router)
+
 	return router
 }
