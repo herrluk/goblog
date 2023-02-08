d55d6e73c6546975dd91f622df80751891ca7250
herrluk <herrluk@example.com>
2023-02-08 15:53:43 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,59 +1,25 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/gorilla/mux"
+	"goBlogByMyself/app/http/middlewares"
 	"goBlogByMyself/bootstrap"
-	"goBlogByMyself/pkg/database"
 	"net/http"
-	"strings"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-// Int64ToString 将 int64 转换为 string
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杆
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
-// Delete 方法用以从数据库中删除单条记录
-
 func main() {
-	database.Initialize()
-	db = database.DB
 
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
-
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
+	router := bootstrap.SetupRoute()
 
 	// 通过命名获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
 	articleURL, _ := router.Get("articles.show").URL("id", "23")
 	fmt.Println("articleURL: ", articleURL)
-	err := http.ListenAndServe(":3000", removeTrailingSlash(router))
+
+	//
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 	if err != nil {
 		fmt.Println(err)
 	}
